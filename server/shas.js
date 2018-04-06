53292ad09ec6cb7345fd26c235addd1d50cbf60b
jongleberry <me@jongleberry.com>
2018-04-05 20:26:40 -0400

    entropy++

--- server/shas.js
+++ server/shas.js
@@ -2,6 +2,6 @@
 const crypto = require('crypto')
 
 exports.createSha = (text) => {
-  if (typeof text !== 'string') throw new TypeError('text must be a string!')
-  return crypto.createHash('sha256').update(text).digest('hex')
+  if (typeof text !== 'string') throw new TypeError('text must be a string!');
+  return crypto.createHash('sha256').update(text).digest('hex');
 }
